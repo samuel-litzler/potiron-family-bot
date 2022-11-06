d91bfce6b4d79a466f081cf235d274f3652d3042
Sam - Surface <samuel@devfamily.fr>
2022-11-06 23:33:29 +0100

    test to receive messages from fb on serv

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.get('/', verificationHandler);
 
+app.post('/',handleMessage);
+
+function handleMessage(req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log(text);
+    }
+  }
+  res.end('received!');
+}
+
 function verificationHandler(req, res) {
   console.log(req);
   if (req.query['hub.verify_token'] === 'verifycode') {
